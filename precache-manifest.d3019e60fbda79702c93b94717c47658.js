65868e72ccb47b1f760cd8421a9632ee448b703c
Roger Perez <rperezga@gmail.com>
2019-01-15 14:33:24 -0500

    Updates

---
+++ precache-manifest.d3019e60fbda79702c93b94717c47658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-your-memory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24987f1f89c669539696",
+    "url": "/test-your-memory/static/js/runtime~main.24987f1f.js"
+  },
+  {
+    "revision": "75598c08b96a9d5169de",
+    "url": "/test-your-memory/static/js/main.75598c08.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/test-your-memory/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "75598c08b96a9d5169de",
+    "url": "/test-your-memory/static/css/main.68389838.chunk.css"
+  },
+  {
+    "revision": "62187237bc6f2e9d40141a3490d194e9",
+    "url": "/test-your-memory/index.html"
+  }
+];
